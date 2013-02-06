96b0cbf35089cc76b53124da90f7fbd3d623827f
MaxHernandez <maxhernandezcastillo@gmail.com>
2013-02-06 12:27:45 -0600

    Tareas de teoria de la informacion

--- tarea1/canales.py
+++ tarea1/canales.py
@@ -1,26 +1,35 @@
 import random, sys, math
 
 class Simulator:
-# frecuencias = [frecuencia de 0, frecuencia de 1]
-# probabilidad de = [[enviar 0 y recibir 0, enviar 0 y recibir1], [enviar 1 y recibir 0, enviar 1 y recibir1]]
-    def __init__(self, frecuencias, probabilidadrecived):
-        self.frecuencias = frecuencias
-        self.Q = probabilidadrecived
+# frequencies = [frequency of 0, frequency of 0 1]
+# probRecived = [[send 0 and recive 0, send 0 and recive 1], [send 1 and recive 0, send 1 y recive 1]]
+    def __init__(self, frequencies, probRecived):
+        self.frequencies = frequencies
+        self.Q = probRecived
 
-    def generate_word(self, longitud):
+    def generate_word(self, lon):
+        """ This function use Bernoulli trials to generato each symbol of
+            a "lon" length word
+	"""
         word = []
-
-        for i in range(longitud):
-            if random.random() > self.frecuencias[0]:
+        for i in range(lon):
+        #Use the frequency vector probabilities to make the Bernoulli trial
+            if random.random() > self.frequencies[0]:
                 word.append(0)
             else:
                 word.append(1)
         return word
 
     def word_transmission(self, word):
+        """ This function use another Bernoulli trial to simulate an error
+            on word transmission
+        """
         word_recived = list()
         for i in range(len(word)):
             aleatorio = random.random()
+            # each symbol has a row in the Q matrix and each row have a 
+            # probability to change or no to change in transmission so that's
+            # what we use to generate the Bernoully trial
             if aleatorio > self.Q[word[i]][word[i]]:
                 if word is 0:
                     word_recived.append(1)
@@ -31,6 +40,7 @@ class Simulator:
         return word_recived
 
     def generate_population(self, lonword, nPopulation):
+	""" This function make transsmition and return the frequency of success"""
         exitos = 0
         for i in range(nPopulation): 
             word = self.generate_word(lonword)
@@ -41,32 +51,56 @@ class Simulator:
         return float(exitos)/nPopulation
 
 def pstr(word):
+    """ Secundary function to obtain a string representation of a vector word"""
     nueva = ""
     for i in word:
         nueva += str(i)
     return nueva
 
 def comp(word1, word2):
+    """Make a word comparison"""
     for i in range(len(word1)):
         if word1[i] != word2[i]:
             return False
     return True
 
+def get_mean(vector):
+    mean = 0.0
+    for i in vector:
+        mean += i
+    return (mean/float(len(vector)))
+
+def get_sd(vector):
+    """Standard deviation """
+    mean = get_mean(vector)
+    sd = 0.0
+    for i in vector:
+        sd += (i-mean)**2
+    sd = (sd/float(len(vector)))**0.5
+    return mean, sd
+
 def main():
     """
     Parametros: 
-    sys.argv[1] -> nombre del archivo
-    sys.argv[2] -> longitud maxima de word
-    sys.argv[3] -> numero de intentos por cada longitud de word
-    sys.argv[4] -> probabilidad de aparicion de 0
-    sys.argv[5] -> probabilidad de enviar un 0 con exito
-    sys.argv[6] -> probabilidad de enviar un 1 con exito
+    sys.argv[1] -> Output file
+    sys.argv[2] -> word maximun length
+    sys.argv[3] -> number of messages sended fo each word length
+    sys.argv[4] -> frequecy of 0 
+    sys.argv[5] -> Send a 0 with success probability
+    sys.argv[6] -> Send a 1 with success probability
     """
     fl = open(sys.argv[1], "w")
     sim = Simulator([float(sys.argv[4]), (1.0-float(sys.argv[4]))], [[float(sys.argv[5]), (1.0-float(sys.argv[5]))], [(1.0-float(sys.argv[6])), float(sys.argv[6])]])
 
+    values = list()
     for i in range(1, int(sys.argv[2])+1):
-        fl.write(str( sim.generate_population(i, int(sys.argv[3])) )+" "+str(i)+"\n")
+        values.append( sim.generate_population(i, int(sys.argv[3])) )
+
+    mean, sd = get_sd(values)
+    for i in range(1, int(sys.argv[2])+1):
+        fl.write(str( values[i-1] )+" "+str(i)+" "+str(mean)+" "+str(sd)+"\n")
+
+
     fl.close()
 
 main()
--- tarea1/noisy_chanels.sh
+++ tarea1/noisy_chanels.sh
@@ -1,26 +1,21 @@
-
-GNUPLOT_FILE="noissy.plot"
 FILE="noissy"
 MAXWORDLEN=50
 SENDREPETITIONS=1000
 
-ZEROPROB="0.50"
-ZEROSEND="0.98"
+ZEROPROB="0.50 0.70 0.90"
+ZEROSEND="0.80"
 ONESEND="0.99"
-#ZEROPROB="0.60 0.78 0.9"
-#ZEROSEND="0.90 0.95 0.99"
-#ONESEND="0.90 0.97 0.99"
-
-> $GNUPLOT_FILE
-echo set term postscript eps color 30 >> $GNUPLOT_FILE
-echo set key outside Right >> $GNUPLOT_FILE
-echo set size 4, 3 >> $GNUPLOT_FILE
-echo set output \"noisy_channel.eps\" >> $GNUPLOT_FILE
-echo set pointsize 2 >> $GNUPLOT_FILE
-echo plot \\ >> $GNUPLOT_FILE
 
 for zp in $ZEROPROB
 do
+    GNUPLOT_FILE="noissy_"$zp".plot"
+    > $GNUPLOT_FILE
+    echo set term postscript eps color 30 >> $GNUPLOT_FILE
+    echo set key outside Right >> $GNUPLOT_FILE
+    echo set size 4, 3 >> $GNUPLOT_FILE
+    echo set output \""noissy_"$zp".eps"\" >> $GNUPLOT_FILE
+    echo set pointsize 3 >> $GNUPLOT_FILE
+    echo plot \\ >> $GNUPLOT_FILE
     for zs in $ZEROSEND
     do
         for os in $ONESEND
@@ -28,7 +23,11 @@ do
 	    python canales.py $FILE$zp\_$zs\_$os".data" $MAXWORDLEN $SENDREPETITIONS $zp $zs $os
 	    echo python canales.py $FILE$zp\_$zs\_$os".data" $MAXWORDLEN $SENDREPETITIONS $zp $zs $os
 	    echo \"$FILE$zp\_$zs\_$os".data"\" using 2:1 with points title \"$zp $zs $os\", \\ >> $GNUPLOT_FILE
-	    echo exp\(-x*\( \($zp*\(1-$zs\)\)+\(\(1-$zp\)*\(1-$os\)\) \)\)  with lines title \"$zp $zs $os esperado\", \\ >> $GNUPLOT_FILE
+	    echo \"$FILE$zp\_$zs\_$os".data"\" using 3 with lines title \"Mean\", \\ >> $GNUPLOT_FILE
+	    echo \"$FILE$zp\_$zs\_$os".data"\" using 4 with impulses title \"Standard deviation\" >> $GNUPLOT_FILE
 	done
     done
 done
+gnuplot *.plot
+rm *.plot
+rm *.data
